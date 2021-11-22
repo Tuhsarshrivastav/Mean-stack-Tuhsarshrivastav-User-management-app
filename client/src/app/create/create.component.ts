d8a3556938ebc802bb6ea9bc344e82c8bd6d8073
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 <ts3657@gmail.com>
2021-11-22 19:41:23 +0530

    added post user

--- client/src/app/create/create.component.ts
+++ client/src/app/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ApiServicesService } from '../api-services.service';
 
 @Component({
@@ -8,9 +9,29 @@ import { ApiServicesService } from '../api-services.service';
 })
 export class CreateComponent implements OnInit {
 
-  constructor(private api:ApiServicesService) { }
+  // userForm!: FormGroup
+
+  constructor(private api: ApiServicesService) { }
+
+  errMsg: any;
+  successMSG: any
 
   ngOnInit(): void {
+  }
+  userForm = new FormGroup({
+    'fullname': new FormControl('', Validators.required),
+    'email': new FormControl('', Validators.required),
+    'mobile': new FormControl('', Validators.required)
+  })
+  userSubmit() {
+    if (this.userForm.valid) {
+      this.api.createData(this.userForm.value).subscribe((res) => {
+        this.successMSG = res.message
+        this.userForm.reset()
+      })
+    } else {
+      this.errMsg = "All Fields Are Reqired"
+    }
   }
 
 }
